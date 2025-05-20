bd1d247eae27fc64de4eadcfb135c8ee50040526
mannuccimattia <mannuccimattia@gmail.com>
2025-05-20 12:33:26 +0200

    [UPD] Done search input - raw, unstyled

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import { useState } from "react"
 
 const App = () => {
+
+  const [searchTerm, setSearchTerm] = useState("")
+
+  console.log(searchTerm)
   return (
     <div>
       <h1>Hello React</h1>
+
+      <label htmlFor="search">Search:</label>
+      <input
+        type="text"
+        id="search"
+        value={searchTerm}
+        onChange={(e) => { setSearchTerm(e.target.value) }}
+      />
+      {searchTerm}
     </div>
   )
 }

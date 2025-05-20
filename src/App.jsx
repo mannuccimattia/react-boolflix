d3201a94f98a89147baba80a1df70df483a67ac1
mannuccimattia <mannuccimattia@gmail.com>
2025-05-20 17:26:02 +0200

    [UPD] Done card image + Fixed lang codes for flag-icons

--- src/App.jsx
+++ src/App.jsx
@@ -45,15 +45,6 @@ const App = () => {
             <div className="col-12 d-flex flex-wrap">
               {list.map(item => (
                 <ListItem key={item.id} item={item} />
-                // <div className="card w-25" key={item.id}>
-                //   <div className="card-title">Titolo: {item.title || item.name}</div>
-                //   <div className="card-title">TitoloOriginale: {item.original_title || item.original_name}</div>
-                //   <div className="card-text">
-                //     Lingua:
-                //     <span className={`fi fi-${item.original_language.toLowerCase()}`}></span>({item.original_language.toUpperCase()})
-                //   </div>
-                //   <div className="card-text">Voto: {item.vote_average}</div>
-                // </div>
               ))}
             </div>
           )}
--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,7 +1,41 @@
 const List = ({ item }) => {
 
+  const imgURL = `https://image.tmdb.org/t/p/w780${item.poster_path}`
+
+  const handleLangFlag = (item) => {
+    const lang = item.original_language.toLowerCase()
+
+    switch (lang) {
+      case "en":
+        return "gb";
+      case "ja":
+        return "jp";
+      case "zh":
+        return "cn";
+      case "ko":
+        return "kr";
+      case "cs":
+        return "cz";
+      case "da":
+        return "dk";
+      default:
+        return lang;
+    }
+
+    // if (lang === "en") {
+    //   return "gb"
+    // }
+    // if (lang === "ja") {
+    //   return "jp"
+    // }
+
+    // return lang.toLowerCase()
+  }
+
   return (
     <div className="card w-25">
+      <img src={imgURL} alt={item.title} className="card-img-top" />
+
       {(item.title || item.name) === (item.original_title || item.original_name) ? (
         <div className="card-title">
           Titolo: {item.title || item.name}
@@ -18,7 +52,7 @@ const List = ({ item }) => {
       )}
       <div className="card-text">
         {`Lingua:`}
-        <span className={`fi fi-${item.original_language.toLowerCase()}`}></span>
+        <span className={`fi fi-${handleLangFlag(item)}`}></span>
         {`(`}{item.original_language.toUpperCase()}{`)`}
       </div>
       <div className="card-text">

57742da6ad997bdd318017a61958da4951357883
mannuccimattia <mannuccimattia@gmail.com>
2025-05-20 14:15:20 +0200

    [UPD] Done show results list on search submit

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,18 @@ const options = {
 
 const App = () => {
 
-  const [searchTerm, setSearchTerm] = useState("")
+  const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState(null);
 
 
   const handleSearchSubmit = (e) => {
     e.preventDefault();
 
-    const endPoint = `https://api.themoviedb.org/3/search/movie?api_key=ddbf93c1fe82b9fa010c3cd4b41c556f&query=${searchTerm}`
+    const endPoint = `https://api.themoviedb.org/3/search/movie?api_key=ddbf93c1fe82b9fa010c3cd4b41c556f&query=${searchTerm}`;
 
-    axios.get(endPoint).then(res => console.log(res.data.results))
-  }
+    axios.get(endPoint).then(res => setMovies(res.data.results));
+  };
 
-  console.log(searchTerm)
   return (
     <>
       <h1>Hello React</h1>
@@ -35,7 +35,24 @@ const App = () => {
         <button>Go!</button>
       </form>
 
-      {searchTerm}
+      <div className="row">
+        <div className="col-12 col-md-6 col-lg-4">
+          {!movies ? (
+            <div>loading</div>
+          ) : (
+            <>
+              {movies.map(movie => (
+                <div className="card" key={movie.id}>
+                  <div className="card-title">Titolo: {movie.title}</div>
+                  <div className="card-title">TitoloOriginale: {movie.original_title}</div>
+                  <div className="card-text">Lingua: {movie.original_language}</div>
+                  <div className="card-text">Voto: {movie.vote_average}</div>
+                </div>
+              ))}
+            </>
+          )}
+        </div>
+      </div>
     </>
   )
 }

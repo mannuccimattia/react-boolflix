6a6db1d38a1916add542e7c01a1c0dfcede57d68
mannuccimattia <mannuccimattia@gmail.com>
2025-05-20 13:57:23 +0200

    [UPD] Done axios call - results in console

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,42 @@
+import axios from "axios";
 import { useState } from "react"
+
+const options = {
+  url: 'https://api.themoviedb.org/3/search/movie?api_key=ddbf93c1fe82b9fa010c3cd4b41c556f&query=ritorno',
+
+};
 
 const App = () => {
 
   const [searchTerm, setSearchTerm] = useState("")
 
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+
+    const endPoint = `https://api.themoviedb.org/3/search/movie?api_key=ddbf93c1fe82b9fa010c3cd4b41c556f&query=${searchTerm}`
+
+    axios.get(endPoint).then(res => console.log(res.data.results))
+  }
+
   console.log(searchTerm)
   return (
-    <div>
+    <>
       <h1>Hello React</h1>
 
-      <label htmlFor="search">Search:</label>
-      <input
-        type="text"
-        id="search"
-        value={searchTerm}
-        onChange={(e) => { setSearchTerm(e.target.value) }}
-      />
+      <form onSubmit={handleSearchSubmit}>
+        <label htmlFor="search">Search:</label>
+        <input
+          type="text"
+          id="search"
+          value={searchTerm}
+          onChange={(e) => { setSearchTerm(e.target.value) }}
+        />
+        <button>Go!</button>
+      </form>
+
       {searchTerm}
-    </div>
+    </>
   )
 }
 
